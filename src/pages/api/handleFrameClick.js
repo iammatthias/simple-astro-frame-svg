9b49579a1476282ce0ca73c1aecd56d5d17b2050
iammatthias <me@iammatthias.com>
2024-01-31 09:04:32 -0800

    add a new svg for the response

--- src/pages/api/handleFrameClick.js
+++ src/pages/api/handleFrameClick.js
@@ -1,15 +1,15 @@
 // src/pages/api/handleFrameClick.js
 
-const svgString =
-  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 630"><rect fill="#fff" width="100%" height="100%"/><foreignObject width="600" height="500" x="300" y="65"><div xmlns="http://www.w3.org/1999/xhtml" style="font-size:24px;font-family:Arial,sans-serif;display:flex;align-items:center;height:100%">This is a new SVG for display in the Frame.</div></foreignObject></svg>';
-const base64EncodedSVG = btoa(svgString);
-
 // Handler for POST requests
 export const POST = async ({ request }) => {
   const requestData = await request.json();
   // Process requestData, verify signature, etc.
 
   console.log(requestData);
+
+  const svgString =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 630"><rect fill="#fff" width="100%" height="100%"/><foreignObject width="600" height="500" x="300" y="65"><div xmlns="http://www.w3.org/1999/xhtml" style="font-size:24px;font-family:Arial,sans-serif;display:flex;align-items:center;height:100%">This is a new SVG for display in the Frame.</div></foreignObject></svg>';
+  const base64EncodedSVG = btoa(svgString);
 
   // Return new frame metadata
   return new Response(
@@ -18,7 +18,7 @@ export const POST = async ({ request }) => {
     <html>
       <head>
         <meta property="fc:frame" content="vNext" />
-        <meta property='fc:frame:image' content="data:image/svg+xml;base64${base64EncodedSVG}" />
+        <meta property='fc:frame:image' content={"data:image/svg+xml;base64," + ${base64EncodedSVG}} />
         <!-- Add more meta tags as needed -->
       </head>
     </html>
